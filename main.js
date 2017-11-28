a801f25a0ddb598e3ed530d4ed91449641f5368b
Ian Agpawa <agpawaji@gmail.com>
2017-11-27 23:51:01 -0500

    feat: Add Todo and TodoList

--- main.js
+++ main.js
@@ -18,6 +18,17 @@ function startPage() {
         );
     };
 
+    const Todo = ({todo, remove}) => {
+        return (<li onclick(remove(todo.id))>{todo.text}</li>);
+    }
+
+    const TodoList = ({todos, remove}) => {
+        const todoNode = todos.map((todo) => {
+            return (<Todo todo={todo} key=todo.id) remove={remove}/>)
+        });
+        return (<ul>{todoNode}</ul>);
+    }
+
 
 }
 
