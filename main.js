168d5a36f37feaa88249565c4a3281c4cedf02d5
Ian Agpawa <agpawaji@gmail.com>
2017-11-27 23:42:33 -0500

    feat: Add TodoForm to js

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 function startPage() {
-    
+    const TodoForm = ({addTodo}) => {
+
+        let input;
+
+        return (
+            <div>
+                <input ref={node => {
+                    input = node;
+                }} />
+                <button onClick={() => {
+                    addTodo(input.value);
+                    input.value = '';
+                }}>
+                    +
+                </button>
+            </div>
+        );
+    };
+
+
 }
 
 $(document).ready(startPage)
