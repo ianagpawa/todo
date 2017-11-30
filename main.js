566607ea31102fdd82cb21520910608ad218ee61
Ian Agpawa <agpawaji@gmail.com>
2017-11-29 23:02:40 -0500

    feat: Add container component

--- main.js
+++ main.js
@@ -39,6 +39,42 @@ function startPage() {
         )
     }
 
+    window.id = 0;
+    class TodoApp extends React.Component{
+        constructor(props){
+            super(props);
+            this.state = {
+                data: []
+            }
+        }
+
+        addTodo(val){
+            const todo = {text: val, id: window.id++}
+            this.state.data.push(todo)
+            this.setState({data: this.state.data});
+        }
+
+        handleRemove(id){
+            const remainder = this.state.data.filter((todo) => {
+                if(todo.id !== id) return todo;
+            });
+            this.setState({data: remainder});
+        }
+
+        render(){
+            return (
+                <div>
+                    <Title />
+                    <TodoForm addTodo={this.addTodo.bind(this)}/>
+                    <TodoList
+                        todos={this.state.data}
+                        remove={this.handleRemove.bind(this)}
+                    />
+                </div>
+            );
+        }
+    }
+
 
 }
 
